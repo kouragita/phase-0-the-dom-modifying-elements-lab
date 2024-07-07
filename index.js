148999ea19cbf24354b5057f55ef5605a8432a39
wilson mwangi waweru <kouragitam@gmail.com>
2024-07-07 17:57:44 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Remove the main element with id 'main'
+const mainElement = document.querySelector('#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+//new <h1> element && newHeader variable
+const newHeader = document.createElement('h1');
+
+//new <h1> element id to 'victory'
+newHeader.id = 'victory';
+
+//innerHTML of the new <h1> element to include "is the champion"
+newHeader.innerHTML = 'Wilson is the champion';
+
+// Append the newHeader
+document.body.appendChild(newHeader);
